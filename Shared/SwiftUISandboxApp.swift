7a57b408e54bf71e1b80876c15ad1a435b14c8f8
Andrew Miotke <andrewmiotke@gmail.com>
2020-06-26 13:11:24 -0700

    Initial commit

---
+++ Shared/SwiftUISandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISandboxApp.swift
+//  Shared
+//
+//  Created by Andrew Miotke on 6/26/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
